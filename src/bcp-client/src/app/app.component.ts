f3a14f8f7c1d9192e5b33c17ed2c2a2ce76f8b8c
YEJIA SHI <yejia.shi@hotmail.com>
2024-11-18 19:09:06 +0100

    remove main menu

--- src/bcp-client/src/app/app.component.ts
+++ src/bcp-client/src/app/app.component.ts
@@ -1,14 +1,9 @@
-import { Component, inject } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
-import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
-import { MatListModule } from '@angular/material/list';
-import { Observable } from 'rxjs';
-import { map, shareReplay } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -18,25 +13,11 @@ import { map, shareReplay } from 'rxjs/operators';
     RouterModule,
     MatToolbarModule,
     MatButtonModule,
-    MatSidenavModule,
     MatIconModule,
-    MatListModule,
   ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  private breakpointObserver = inject(BreakpointObserver);
-
-  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
-    .pipe(
-      map(result => result.matches),
-      shareReplay()
-    );
-
-  handleMenuClick(sidenav: any): void {
-    if (this.breakpointObserver.isMatched(Breakpoints.Handset)) {
-      sidenav.close();
-    }
-  }
+  // 移除所有与侧边栏相关的代码
 }
